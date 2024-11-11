b1a95d65fc32bdea0a695a1416b6a2953ef84417
fcaretti01 <fcaretti@mit.edu>
2024-11-11 02:00:01 -0500

    Automated update from dataloader

--- tools/latest_papers/dataloaders_papers.py
+++ tools/latest_papers/dataloaders_papers.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import sqlite3
 import os
 import sys
 from database_manager import DatabaseManager
